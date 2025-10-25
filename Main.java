c0458e62e7adb28b8c3bcd17988ee3e84a054ad3
smithexauceyayama <exauceyayama@gmail.com>
2025-10-25 12:11:27 +0100

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Salut Exaucé, Java tourne sur VS Code !");
-    }
-}
+public class 
---
+++ Source.java
@@ -0,0 +1,5 @@
+public class Source{
+    public static void main(String[] args){
+        System.out.println("Je suis SMITH EXAUCE YAYAMA");
+    }
+}
