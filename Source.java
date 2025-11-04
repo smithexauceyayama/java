c8ed3ef94a983e2985c9a0489150ecc63eb446e7
smithexauceyayama <exauceyayama@gmail.com>
2025-11-04 10:19:40 +0100

    update

--- Source.java
+++
@@ -1,6 +0,0 @@
-public class Source{
-    public static void main(String[] args){
-        String name = "SMITH EXAUCE YAYAMA";
-        System.out.println("Je suis " + name);
-    }
-}
---
+++ exercice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//ecrivez un propgramme qui demande un noimbfre a l'utilisateur et afficge si il le positif ou negatif ou null
+public class exercice {
+    public static void main(String[] args) {
+        int nombre;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        nombre = scanner.nextInt();
+        
+        if (nombre > 0){
+            System.out.println
+        }
+    }
+}
